02ab024332c2e5c9658f559b4795d62019cabc66
Kunal Nagar <knlnagar@gmail.com>
2020-03-24 19:31:18 -0400

    ESLint config fixed

--- index.js
+++ index.js
@@ -1,38 +1,147 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
   extends: [
     'eslint:recommended',
     'react-app',
-    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
     'prettier/react',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  // eslint-plugin-react-app adds their rules to overrides, so in order to
+  // override them, we need to use overrides as well
+  overrides: [
+    {
+      files: ['src/**/*.ts', 'src/**/*.tsx'],
+      rules: {
+        '@typescript-eslint/camelcase': [
+          'error',
+          {
+            ignoreDestructuring: true,
+            properties: 'never',
+          },
+        ],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/generic-type-naming': [
+          'error',
+          '^T[A-Z][a-zA-Z]+$',
+        ],
+        '@typescript-eslint/interface-name-prefix': ['error', 'always'],
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-extraneous-class': 'error',
+        '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+          },
+        ],
+      },
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
+  ],
   rules: {
+    'default-case': 'error',
+    eqeqeq: 'error',
     'linebreak-style': ['error', 'unix'],
     'no-console': 'warn',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
+    'no-debugger': 'warn',
+    'no-duplicate-imports': 'error',
+    'no-lonely-if': 'error',
+    'no-magic-numbers': [
+      'error',
+      {
+        ignore: [0, 1, 2],
+      },
+    ],
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            message: 'Please import from styled-components/macro.',
+            name: 'styled-components',
+          },
+        ],
+        patterns: ['!styled-components/macro'],
+      },
+    ],
+    // Disabled because this flags styled-components multiline template literals
+    'no-unexpected-multiline': 'off',
+    'no-unneeded-ternary': 'error',
+    'no-useless-computed-key': 'error',
+    'no-useless-rename': 'error',
+    'object-shorthand': 'error',
+    'padded-blocks': ['error', 'never'],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        next: 'return',
+        prev: '*',
+      },
+      {
+        blankLine: 'always',
+        next: 'block-like',
+        prev: '*',
+      },
+      {
+        blankLine: 'always',
+        next: '*',
+        prev: 'block-like',
+      },
+      {
+        blankLine: 'always',
+        next: 'break',
+        prev: '*',
+      },
+      {
+        blankLine: 'always',
+        next: 'continue',
+        prev: '*',
+      },
+    ],
+    'prefer-arrow-callback': [
+      'error',
+      {
+        allowNamedFunctions: true,
+      },
+    ],
+    'prefer-const': 'warn',
+    'prefer-destructuring': 'error',
+    'prefer-object-spread': 'error',
+    'prefer-rest-params': 'error',
+    'react/jsx-handler-names': 'error',
+    'react/sort-comp': [
+      'error',
+      {
+        order: [
+          'type-annotations',
+          'instance-variables',
+          'static-methods',
+          'lifecycle',
+          'everything-else',
+          '/^render.+$/',
+          'render',
+        ],
+      },
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
+    'sort-keys': [
+      'error',
+      'asc',
+      {
+        natural: true,
+      },
+    ],
+    'sort-vars': 'error',
+    yoda: 'error',
   },
 }
